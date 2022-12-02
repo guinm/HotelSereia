8060bde75b589bbf487abffceb7074656661deb4
JustG <guimachado94@gmail.com>
2022-12-02 18:00:27 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function SocialToggle() {
 	var ST = document.getElementById("SideMenu");
 	ST.classList.toggle("open");
 }
+
+function SocialFocus(FocusedSocial) {
+	var ST = document.getElementById("SideMenu");
+	if (FocusedSocial == document.activeElement) {
+		ST.classList.add("open");
+	} else {
+		ST.classList.remove("open");
+	}
+}
